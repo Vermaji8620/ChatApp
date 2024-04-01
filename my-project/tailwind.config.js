f8e73e9de8083db21b5d773d3bbdcb415ec12aa8
vermaji <rajaditya14032002@gmail.com>
2024-04-01 20:03:14 +0530

    tailwind + daisyUI connection done

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,15 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx,vue}",
+    "./src/**/*",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
