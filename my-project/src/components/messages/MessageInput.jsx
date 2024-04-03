6ab682786e10ac5611edd1439cbb88df33c6a588
vermaji <rajaditya14032002@gmail.com>
2024-04-03 09:06:38 +0530

    send message functionality working from frontend

--- my-project/src/components/messages/MessageInput.jsx
+++ my-project/src/components/messages/MessageInput.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import { IoSend } from "react-icons/io5";
+import useSendMessage from "../../hooks/useSendMessage";
 
 const MessageInput = () => {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full flex gap-3">
         <input
           type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
           placeholder="Send a message"
         />
-
         <button className="btn btn-outline btn-primary">
-          <IoSend />
+          {loading ? (
+            <span className="loading loading-spinner loading-lg"></span>
+          ) : (
+            <IoSend />
+          )}
         </button>
       </div>
     </form>
