ebee41b0d813f707a03543854e8bd090bca9d3b9
vermaji <rajaditya14032002@gmail.com>
2024-04-03 17:55:22 +0530

    bell notification done, now deploying

--- my-project/src/hooks/useListenMessages.js
+++ my-project/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
+import NotificationSound from "../assets/notification.mp3";
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
@@ -8,6 +9,8 @@ const useListenMessages = () => {
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
+      const noti = new Audio(NotificationSound);
+      noti.play();
       setMessages([...messages, newMessage]);
     });
 
