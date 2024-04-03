7dff793c9c76db42b81c828d83807e68845b1c02
vermaji <rajaditya14032002@gmail.com>
2024-04-03 18:20:46 +0530

    changes and deployment building done, now hosting on render

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,24 @@ dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
+import path from "path";
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 import mongoose from "mongoose";
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users/", userRoutes);
+
+// express.static is a built-in middleware function in Express. It serves static files and is based on serve-static.
+app.use(express.static(path.join(__dirname, "/my-project/dist")));
+
+// niche wala code is for production...
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "my-project", "dist", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGO_DB_URI)
