33f170b1f052e743653fc9fcd11d50ad7fbdafef
vermaji <rajaditya14032002@gmail.com>
2024-05-12 18:02:34 +0530

    user profile image updated

--- my-project/src/components/sidebar/Conversation.jsx
+++ my-project/src/components/sidebar/Conversation.jsx
@@ -18,7 +18,10 @@ const Conversation = ({ conversation }) => {
       >
         <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
-            <img src={conversation.profilePic} alt="user avatar" />
+            <img
+              src={conversation.profilePic}
+              alt="https://th.bing.com/th/id/OIP.cSPj7LvJYHZOnzdIyFok2gHaHT?w=198&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+            />
           </div>
         </div>
         <div className="flex flex-col flex-1">
